fffe1ba9e239a6e8460a8b2596432ca6e099ed1a
jk <jk@mail.com>
2019-10-11 16:20:31 +0530

    Add Controller.......

---
+++ CoreMVC_CRUD/Migrations/20191011100556_initial Migration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreMVC_CRUD.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    FullName = table.Column<string>(nullable: true),
+                    EmpCode = table.Column<string>(nullable: true),
+                    Position = table.Column<string>(nullable: true),
+                    OfficeLocation = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ CoreMVC_CRUD/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC_CRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+
+        public string FullName { get; set; }
+
+        public string EmpCode { get; set; }
+
+        public string Position { get; set; }
+
+        public string OfficeLocation { get; set; }
+    }
+}
